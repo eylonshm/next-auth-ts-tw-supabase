26816cacadefce070d0dcd581875fa81870af476
eylonshm <eylonshm@gmail.com>
2024-04-13 12:31:36 +0300

    Create atomicDesign folders

---
+++ src/app/components/atoms/index.ts
@@ -0,0 +1,2 @@
+export { Input, type InputProps } from "./Input/input";
+export { Button, type ButtonProps } from "./Button/button";
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,1 @@
-export { Input, type InputProps } from "./Input/input";
-export { Button, type ButtonProps } from "./Button/button";
+export * from "./atoms";
