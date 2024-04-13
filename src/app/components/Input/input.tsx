333f953839da876ac58cf3767eafb922cb2efdee
eylonshm <eylonshm@gmail.com>
2024-04-13 12:17:38 +0300

    Conigure Storybook to work with tailwindCSS

--- src/app/components/Input/input.tsx
+++ src/app/components/Input/input.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+"use client";
 
+import React from "react";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ onChange }: InputProps) => {
-  return <input type="text" />;
+export const Input = ({ onChange, ...rest }: InputProps) => {
+  return (
+    <input
+      type="text"
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-green-500"
+      {...rest}
+    />
+  );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import { Input } from "./components";
+
+const EMAIL_INPUT_ID = "email";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center">
-        <label>Email</label>
-        <input type="email" placeholder="Enter your email" />
+        <label htmlFor={EMAIL_INPUT_ID}>Email</label>
+        <Input id={EMAIL_INPUT_ID} />
       </div>
     </main>
   );
