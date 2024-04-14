f0ff5dd638cfad45d67e8b618a37d5cef3ebe0fd
eylonshm <eylonshm@gmail.com>
2024-04-14 19:38:35 +0300

    Remove unnecessry code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
 import { NavBar } from "./components/organisms";
-import { createServerClient } from "./utils";
 import { AuthProvider } from "./HOC";
 
 const inter = Inter({ subsets: ["latin"] });
